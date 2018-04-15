6ffde2f07f4dac7e8d68d691f5d2bd29c738cdcf
Victor <victor@foursquare.com>
2018-04-15 19:13:56 -0400

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: 'Victor Chen',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-55579054-1',
+        head: false,
+        anonymize: true,
+        respectDNT: true
+      }
+    }
+  ]
 }
