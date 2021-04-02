c66ce2d8b7721d505dceaacef4de0c1533742d98
Victor Chen <vichen@fb.com>
2021-04-02 00:43:12 -0400

    update interests

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,10 @@ import IncreaseImg from '../assets/increase.png';
 import GlobeImg from '../assets/globe.png';
 import DogImg from '../assets/dog.png';
 import CodeImg from '../assets/code.png';
-import TvImg from '../assets/tv.png';
+import TrollImg from '../assets/troll.png';
 import CardsImg from '../assets/cards.png';
-import PoppingImg from '../assets/popping.png';
+import AnimeImg from '../assets/anime.png';
+import AndroidImg from '../assets/android.png';
 import './index.css';
 
 const Index = () => (
@@ -30,13 +31,14 @@ const Index = () => (
           renderContent={
             <GroupBlock title="Interests">
               <Tag icon={SnowboarderImg} text="Snowboarding" />
-              <Tag icon={IncreaseImg} text="Stocks" />
+              <Tag icon={IncreaseImg} text="Stonks" />
               <Tag icon={GlobeImg} text="Frontend Development" />
               <Tag icon={DogImg} text="Animals" />
               <Tag icon={CodeImg} text="Javascript" />
-              <Tag icon={TvImg} text="Westworld" />
-              <Tag icon={CardsImg} text="Card Magic" />
-              <Tag icon={PoppingImg} text="Popping" />
+              <Tag icon={TrollImg} text="Memes" />
+              <Tag icon={CardsImg} text="Magic" />
+              <Tag icon={AnimeImg} text="Anime" />
+              <Tag icon={AndroidImg} text="Android" />
             </GroupBlock>
           }
         />
