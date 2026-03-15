9ae281f5839210b4aa9a8faf11279e5e6c1c1bed
Victor Chen <victorchen@Victors-MacBook-Pro.local>
2026-03-15 16:13:26 -0400

    Updates

---
+++ _next/static/chunks/app/layout-1f08f8dcc3f2b63c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1290:()=>{},1390:e=>{e.exports={style:{fontFamily:"'Geist', 'Geist Fallback'",fontStyle:"normal"},className:"__className_188709"}},7301:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,1390,23)),Promise.resolve().then(l.t.bind(l,1290,23))}},e=>{e.O(0,[988,441,255,358],()=>e(e.s=7301)),_N_E=e.O()}]);
